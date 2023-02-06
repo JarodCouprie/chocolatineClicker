925a69d224bc57666116115f110ba42e7ee566cb
Spidw3b <jarod64@hotmail.fr>
2023-02-06 16:59:48 +0100

    fonctions

--- asset/script/main.js
+++ asset/script/main.js
@@ -1,2 +1,24 @@
 //logique
 
+class UpgradeItem{
+    constructor (imageItem, nameItem, price, countItem, idItem, imagePath, imageAlternativeText){
+        this.imageItem = imageItem;
+        this.nameItem = nameItem;
+        this.price = price;
+        this.countItem = countItem;
+        this.idItem = idItem;
+        this.imagePath = imagePath;
+        this.imageAlternativeText = imageAlternativeText;
+    }
+    addToPage(){
+        let item = document.createElement("li");
+        let itemList = document.querySelector("#container");
+        item.classList.add("upgrade");
+        item.setAttribute("id",this.idItem);
+        let imageItem = document.createElement("img");
+        imageItem.setAttribute("src",this.imagePath);
+        imageItem.setAttribute("alt",this.imageAlternativeText);
+        item.appendChild(imageItem);
+        let upgradePriceTag = document.createElement
+    }
+}
